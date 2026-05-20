206207fdbd7db88e6cff9f6e9381f85d5b478998
Saswath Yeshwanth <36449546+saswath-06@users.noreply.github.com>
2026-05-20 17:24:58 -0400

    Add: two-integer-sum - submission-30

---
+++ Data Structures & Algorithms/two-integer-sum/submission-30.py
@@ -0,0 +1,8 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        num_dict = {}
+        for i, n in enumerate(nums):
+            diff = target - n
+            if diff in num_dict:
+                return [num_dict.get(diff), i]
+            num_dict[n] = i
